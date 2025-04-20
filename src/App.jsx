235673e296b0d764fd692b328b6dc6db0de28003
Manoj <manoj.balineni04@gmail.com>
2025-04-20 09:59:45 +0530

    vite and configure tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 font-bold">Building Netlfix gpt</h1>
+    </div>
+  );
+}
+
+export default App;
